12ab9a7a8a27ea402848827b63aff5de7dcda0a2
zaighy <m.zaighumali@gmail.com>
2018-05-18 12:05:42 +0500

    Cleared User Type Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum user_type: [ :developer, :manager, :qa ]
+  enum user_type: %i[ Developer Manager QA ]
 end
